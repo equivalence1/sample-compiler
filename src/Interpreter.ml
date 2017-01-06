ae527ce1e9547936cb3178d7e3287ea948c86d26
equivalence1 <equivalence1@gmail.com>
2017-01-06 03:45:09 +0300

    parsing

--- src/Interpreter.ml
+++ src/Interpreter.ml
@@ -17,7 +17,7 @@ module Expr =
             func args' (fmap, [])
 
     end
-  
+
 module Stmt =
     struct
 
@@ -25,42 +25,42 @@ module Stmt =
         open Utils
 
         let rec eval' env stmt =
-                match stmt with
-                | Skip          -> (`Continue, env)
-                | Seq    (l, r) -> 
-                    (let (result, env') = eval' env l in
-                    match result with
-                    | `Return x -> (`Return x, env')
-                    | `Continue -> eval' env' r)
-                | Assign (x, e) ->
-                    let value = Expr.eval env e in
-                    (`Continue, InterpreterEnv.set_var x value env)
-                | Write   e     ->
-                    let value = Expr.eval env e in
-                    Printf.printf "%d\n" value;
+            match stmt with
+            | Skip          -> (`Continue, env)
+            | Seq    (l, r) -> 
+                (let (result, env') = eval' env l in
+                match result with
+                | `Return x -> (`Return x, env')
+                | `Continue -> eval' env' r)
+            | Assign (x, e) ->
+                let value = Expr.eval env e in
+                (`Continue, InterpreterEnv.set_var x value env)
+            | Write   e     ->
+                let value = Expr.eval env e in
+                Printf.printf "%d\n" value;
+                (`Continue, env)
+            | Read    x     ->
+                Printf.printf "> ";
+                let value = read_int () in
+                (`Continue, InterpreterEnv.set_var x value env)
+            | If (e, s1, s2) ->
+                let result = Expr.eval env e in
+                if result <> 0 then
+                    eval' env s1
+                else
+                    eval' env s2
+            | While (e, s)  ->
+                let result = Expr.eval env e in
+                if result <> 0 then
+                    eval' env (Seq (s, While (e, s)))
+                else
                     (`Continue, env)
-                | Read    x     ->
-                    Printf.printf "> ";
-                    let value = read_int () in
-                    (`Continue, InterpreterEnv.set_var x value env)
-                | If (e, s1, s2) ->
-                    let result = Expr.eval env e in
-                    if result <> 0 then
-                        eval' env s1
-                    else
-                        eval' env s2
-                | While (e, s)  ->
-                    let result = Expr.eval env e in
-                    if result <> 0 then
-                        eval' env (Seq (s, While (e, s)))
-                    else
-                        (`Continue, env)
-                | Return  e     ->
-                    let result = Expr.eval env e in
-                    (`Return result, env)
-                | Call (name, args) ->
-                    let result = Expr.eval env (Call (name, args)) in
-                    (`Continue, env)
+            | Return  e     ->
+                let result = Expr.eval env e in
+                (`Return result, env)
+            | Call (name, args) ->
+                let result = Expr.eval env (Call (name, args)) in
+                (`Continue, env)
 
         let eval func_list main =
             let env = InterpreterEnv.init_env in
@@ -70,7 +70,7 @@ module Stmt =
                     let env_in_func = (fmap, func_state) in
                     let (res, env_after_call) = eval' env_in_func func_stmt in
                     match res with
-                    | `Continue -> failwith "void functions are not supported"
+                    | `Continue -> 0
                     | `Return x -> x
                 in
                 InterpreterEnv.add_func name func cur_env
@@ -86,6 +86,55 @@ module Prog =
 
         let eval prog = 
             match prog with
-            | (funcs, main) -> Stmt.eval funcs main; ()
+            | (classes, funcs, main) -> Stmt.eval funcs main; ()
+
+
+        open Language.Expr
+        open Language.Stmt
+
+        let rec print_expr e =
+            match e with
+            | Const n -> Printf.printf "%d" n;
+            | Var x   -> Printf.printf "%s" x;
+            | BinOp (op, x, y) -> print_expr x; Printf.printf "%s" op; print_expr y
+            | Call (name, args) -> Printf.printf "%s(" name; List.iter (fun arg -> print_expr arg; Printf.printf ", ") args; Printf.printf ")"
+            | New (cls, args) -> Printf.printf "new %s(" cls; List.iter (fun arg -> print_expr arg; Printf.printf ", ") args; Printf.printf ")"
+            | MCall (obj, m, args) -> Printf.printf "%s.%s(" obj m; List.iter (fun arg -> print_expr arg; Printf.printf ", ") args; Printf.printf ")"
+
+        let rec print_stmt stmt =
+            match stmt with
+            | Skip           -> Printf.printf "\n"
+            | Seq    (l, r)  -> print_stmt l; Printf.printf "\n"; print_stmt r
+            | Ref (tp, x)    -> Printf.printf "%s %s" tp x
+            | Assign (x, e)  -> Printf.printf "%s := " x; print_expr e; Printf.printf "\n"
+            | Write e        -> Printf.printf "write("; print_expr e; Printf.printf ")\n"
+            | Read x         -> Printf.printf "read(%s)\n" x
+            | If (e, s1, s2) -> Printf.printf "if ("; print_expr e; Printf.printf ")\n"; print_stmt s1; Printf.printf "else\n"; print_stmt s2; Printf.printf "fi\n"
+            | While (e, s)   -> Printf.printf "while ("; print_expr e; Printf.printf ")\n"; print_stmt s
+            | Return e       -> Printf.printf "return "; print_expr e; Printf.printf "\n"
+            | Call (name, args) -> Printf.printf "%s(" name; List.iter (fun arg -> print_expr arg; Printf.printf ", ") args; Printf.printf ")\n"
+            | MCall (obj, m, args) -> Printf.printf "%s.%s(" obj m; List.iter (fun arg -> print_expr arg; Printf.printf ", ") args; Printf.printf ")"
+
+        let print_fun f =
+            let (name, (params, stmt)) = f in
+            Printf.printf "%s(" name;
+            List.iter (fun (tp, x) -> Printf.printf "%s %s, " tp x) params;
+            Printf.printf ")\n{";
+            print_stmt stmt;
+            Printf.printf "}"
+
+        let print_class cls =
+            let (name, fields, methods) = cls in
+            Printf.printf "Class %s:\n  Fields:\n" name;
+            List.iter (fun (tp, name) -> Printf.printf "    %s %s\n" tp name) fields;
+            Printf.printf "  Methods:\n";
+            List.iter (fun meth -> print_fun meth) methods;
+            Printf.printf "\n\n"
+
+        let print prog =
+            let (classes, funcs, main) = prog in
+            List.iter (fun cls -> print_class cls) classes;
+            List.iter (fun f -> print_fun f) funcs;
+            print_stmt main
 
     end
--- src/Language.ml
+++ src/Language.ml
@@ -9,6 +9,8 @@ module Expr =
     | Var   of string
     | BinOp of string * t * t
     | Call  of string * t list
+    | New   of string * t list
+    | MCall of string * string * t list
 
     ostap (
       parse:
@@ -29,14 +31,31 @@ module Expr =
       primary:
         n:DECIMAL {Const n}
       | f:IDENT args:(-"(" !(Util.list0 parse) -")")? {
-	  match args with 
-	  | None      -> Var f 
-	  | Some args -> Call (f, args)
-        }      
+	        match args with 
+	        | None      -> Var f 
+	        | Some args -> Call (f, args)
+        }
+      | %"new" c:IDENT "(" args:!(Util.list0 parse) ")" {
+            New (c, args)
+        }
+      | obj:IDENT "." m:IDENT "(" args:!(Util.list0 parse) ")" {
+            MCall (obj, m, args)
+        }
       | -"(" parse -")"
     )
 
   end
+
+module ReferenceDef =
+    struct
+
+        type t = string * string
+
+        ostap (
+            parse: tp:IDENT x:IDENT {(tp, x)}
+        )
+
+    end
 
 module Stmt =
   struct
@@ -51,6 +70,8 @@ module Stmt =
     | While  of Expr.t * t
     | Call   of string * Expr.t list
     | Return of Expr.t
+    | Ref    of string * string
+    | MCall  of string * string * Expr.t list
 
     ostap (
       parse: s:simple d:(-";" parse)? {match d with None -> s | Some d -> Seq (s, d)};
@@ -59,6 +80,10 @@ module Stmt =
         x:IDENT s:(":=" e:expr {Assign (x, e)} | 
                    "(" args:!(Util.list0 expr) ")" {Call (x, args)}
                   ) {s}
+      | obj:IDENT "." m:IDENT "(" args:!(Util.list0 Expr.parse) ")" {
+            MCall (obj, m, args)
+        }
+      | rf:!(ReferenceDef.parse) {let (a, b) = rf in Ref (a, b)}
       | %"read"  "(" x:IDENT ")" {Read x}
       | %"write" "(" e:expr  ")" {Write e}
       | %"skip"                  {Skip}
@@ -78,19 +103,19 @@ module Stmt =
       | %"while"  e:expr %"do"  body:parse %"od" {While (e, body)}
       | %"repeat" s:parse %"until" e:expr {Seq (s, While (BinOp ("==", e, Const 0) , s))}
       | %"for" i:parse "," c:expr "," s:parse %"do" b:parse %"od" {
-	  Seq (i, While (c, Seq (b, s)))
+	        Seq (i, While (c, Seq (b, s)))
         }
     )
 
   end
 
-module Def =
+module FunDef =
   struct
 
-    type t = string * (string list * Stmt.t)
+    type t = string * (ReferenceDef.t list * Stmt.t)
 
     ostap (
-      arg  : IDENT;
+      arg  : !(ReferenceDef.parse);
       parse: %"fun" name:IDENT "(" args:!(Util.list0 arg) ")" %"begin" body:!(Stmt.parse) %"end" {
         (name, (args, body))
       }
@@ -98,13 +123,32 @@ module Def =
 
   end
 
+module ClassDef =
+    struct
+
+        type t = string * ReferenceDef.t list * FunDef.t list
+
+        ostap (
+            parse:
+            %"class" name:IDENT (* TODO extends *)
+            %"begin"
+            fields: (!(ReferenceDef.parse) -";")*
+            _methods: !(FunDef.parse)*
+            %"end"
+            {
+                (name, fields, _methods)
+            }
+        )
+
+    end
+
 module Unit =
   struct
 
-    type t = Def.t list * Stmt.t
+    type t = ClassDef.t list * FunDef.t list * Stmt.t
 
     ostap (
-      parse: !(Def.parse)* !(Stmt.parse)
+      parse: !(ClassDef.parse)* !(FunDef.parse)* !(Stmt.parse)
     )
 
   end
